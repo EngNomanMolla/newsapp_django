cff8c7d323db17b960e6a0d3647182c585c1858f
EngNomanMolla <mollanoman2019@gmail.com>
2023-05-19 20:00:46 +0600

    update host allowed

--- pratice1/settings.py
+++ pratice1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m)n20w2)qu+@fi72&5cxk7=_ct64zbfbanm1b_l@i+jx4k!jt0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
